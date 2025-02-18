a170af52aa17d4f8ee871a81ab9f902cc3b0e560
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2025-02-18 14:01:59 +0530

    first commit

---
+++ src/main/java/com/example/iManager/IManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.iManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IManagerApplication.class, args);
+	}
+
+}
